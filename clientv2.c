1b72cf5013664d054d2628472f8a3bc19bb6737b
chaos20 <33138600+chaos20@users.noreply.github.com>
2019-03-17 14:58:19 -0600

    Se añade la funcion de cambiar el color del texto
    El texto que venga de fuera del cliente tiene otro color (amarrillo)

--- clientv2.c
+++ clientv2.c
@@ -6,9 +6,51 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+//#include "ini.h"
 
-#define PORT 4444
+//#define PORT
 
+
+//seleccionen uno de estos colores en el printf del mensaje
+// obtenido de: https://stackoverflow.com/questions/3585846/color-text-in-terminal-applications-in-unix
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+
+//parsing de el archivo config para el puerto del client
+//se utiliza la libreria inih
+//recuperado de: https://github.com/benhoyt/inih
+
+// para complilar: gcc clientv2.c ini.c -o a
+/*
+typedef struct
+{
+    int Usport;
+} configuration;
+
+static int handler(void* port, const char* section, const char* name, const char* value)
+  {
+    configuration* pconfig = (configuration*)port;
+    #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+    if (MATCH("port", "clientport")) {
+        pconfig->Usport = atoi(value);
+    }
+    else{
+      return 0;
+    }
+    return 1;
+  }
+  */
+// fin del codigo del parser
+
+
+// funcion para enviar texto de tamaño más grande que una palabra
 char* scan_line(char *line)
 {
     int ch; //as getchar() returns `int`
@@ -40,8 +82,18 @@ char* scan_line(char *line)
 // get line code
 
 
-int main(){
+int main(int argc, char* argv[]){
+/*
+  configuration config;
+  if (ini_parse("test.ini", handler, &config) < 0) {
+        printf("Can't load 'test.ini'\n");
+        return 1;
+  }
+  printf("Config loaded from 'test.ini': port=%d\n",
+        config.Usport);
 
+  //PORT = config.Usport;
+  */
 	int clientSocket, ret;
 	struct sockaddr_in serverAddr;
 	char buffer[1024];
@@ -55,7 +107,7 @@ int main(){
 
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_port = htons(config.Usport);
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
@@ -79,7 +131,7 @@ int main(){
 		if(recv(clientSocket, buffer, 1024, 0) < 0){
 			printf("[-]Error in receiving data.\n");
 		}else{
-			printf("Server: \t%s\n", buffer);
+			printf(YEL"Server: \t%s"RESET"\n", buffer);
 		}
 	}
 
